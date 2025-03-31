1bd852d035006626be2eedd96c8a57c11e8b25e9
MoisesAmen <0512020014@alumnos.unp.edu.pe>
2025-03-31 00:45:31 -0500

    Agregar controlador de Dashboard y modelos para gestión de proyectos, incluyendo vistas para crear y listar catálogos.

---
+++ views/dashboard/formulario-proyecto.php
@@ -0,0 +1,9 @@
+<div class="campo">
+    <label for="proyecto">Nombre Catalogo</label>
+    <input
+        type="text"
+        name="proyecto"
+        id="proyecto"
+        placeholder="Nombre del Catalogo"
+    />
+</div>
